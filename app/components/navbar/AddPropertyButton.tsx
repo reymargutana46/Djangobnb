ed00e99bb8c66dd6d7eb144ce3ffafcb9cc79b7d
unknown <reymargutana46.com>
2025-11-10 15:32:32 +0800

    Code for nav bar

---
+++ app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertyButton = () => {
+    return (
+        <div className="p-2 cursor-pointer text-sm font-semibold rouded-full hover:bg-gray-200">
+            Djangobnb your home
+        </div>
+    )
+}
+
+export default AddPropertyButton;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="">
       Django Bnb
+
+      <h2 className="text-airbnb">Django and next rules</h2>
     </main>
   );
 }
