6620e1664d928e5e61dd04f636b5a427eab48dad
unknown <reymargutana46.com>
2025-11-12 02:07:58 +0800

    Last in the first video - Detail page fo properties(static) - Reymar C. Gutana

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
+
+const PropertyDetailPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+        <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+            <Image
+                fill
+                src="/beach_1.jpg"
+                className="object-cover w-full h-full"
+                alt="Beach house"
+            />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+            <div className="py-6 pr-6 col-span-3">
+                <h1 className="mb-4 text-4xl">Property name</h1>
+
+                <span className="mb-6 block text-lg text-gray-600">
+                    4 guests - 2 bedrooms - 1 bathroom
+                </span>
+
+                <hr />
+
+                <div className="py-6 flex items-center space-x-4">
+                    <Image
+                        src="/profile_pic_1.jpg"
+                        width={50}
+                        height={50}
+                        className="rounded-full"
+                        alt="The user name"
+                    />
+
+                    <p><strong>John Doe</strong> is your host</p>
+                </div>
+
+                <hr />
+
+                <p className="mt-6 text-lg">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Hic ipsa, fuga officiis magnam eius voluptatum, quas omnis accusamus sed 
+                    blanditiis facilis consequatur unde tempore! Dicta commodi cum deleniti error sed!
+                </p>
+            </div>
+
+            <ReservationSidebar />
+        </div>
+    </main>
+    
+  )
+};
+
+export default PropertyDetailPage;
