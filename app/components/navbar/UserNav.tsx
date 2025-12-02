193f6ef61146fb4968b105cd5417265596a9b94d
unknown <reymargutana46.com>
2025-12-02 13:31:04 +0800

    Log in modal(remember error codes) - Gutana, Reymar C.

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 
 import MenuLink from "./MenuLink";
 
+import useLoginModal from "@/app/hooks/useLoginModal";
+
 const UserNav = () => {
+    const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false)
 
     return (
@@ -26,7 +29,12 @@ const UserNav = () => {
                 <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
                     <MenuLink 
                         label='Log in'
-                        onClick={() => console.log('Clicked button')}
+                        onClick={() => {
+                            console.log('Clicked button')
+
+                            setIsOpen(false);
+                            loginModal.open();
+                        }}
                     />
 
                     <MenuLink 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
-import Modal from "./components/modals/Modal";
+import LoginModal from "./components/modals/LoginModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +27,8 @@ export default function RootLayout({
         <div className="pt-32">
           {children}
         </div>
-        
+
+        <LoginModal />
       </body>
     </html>
   );
