09bc069532b9dbcd5c0e024181000f0d58eac881
unknown <reymargutana46.com>
2025-12-12 02:00:31 +0800

    Get properties in the front end and list them - Gutana, Reymar C.

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-const PropertyListItem = () => {
+import { PropertyType }from "./PropertyList";
+
+interface PropertyProps {
+    property: PropertyType
+}
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property
+}) => {
     return (
         <div className="cursor-pointer">
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
                     fill
-                    src='/beach_1.jpg'
+                    src={property.image_url}
                     sizes="(max-width: 768px) 768, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach House"
@@ -13,11 +21,11 @@ const PropertyListItem = () => {
             </div>
 
             <div className="mt-2">
-                <p className="text-lg font-bold">Property Name</p>
+                <p className="text-lg font-bold">{property.title}</p>
             </div>
 
             <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>$200</strong>per night</p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong>per night</p>
             </div>
         </div>
     )
